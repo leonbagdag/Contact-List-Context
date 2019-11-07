806bc853a49a0ba9a9b2951197622abfb63dd107
leonbagdag <dsrt1991@gmail.com>
2019-11-07 19:15:27 +0000

    flux y context

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -7,40 +7,34 @@ export const { Consumer, Provider } = React.createContext(null);
 // This function injects the global store to any view/component where you want to use it, we will inject the context to Layout.jsx, you can see it here:
 // https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.jsx#L35
 const injectContext = PassedComponent => {
-	const StoreWrapper = props => {
-		//this will be passed as the contenxt value
-		const [state, setState] = useState(
-			getState({
-				getStore: () => state.store,
-				getActions: () => state.actions,
+	class StoreWrapper extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = getState({
+				getStore: () => this.state.store,
 				setStore: updatedStore =>
-					setState({
-						store: Object.assign(state.store, updatedStore),
-						actions: { ...state.actions }
+					this.setState({
+						store: Object.assign(this.state.store, uppdatedStore)
 					})
-			})
-		);
-
-		useEffect(() => {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only run once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here
-			 *
-			 * state.loadSomeData(); <---- calling this function from the flux.js actions
-			 *
-			 **/
-		}, []);
-
-		// the initial value for the context its not null anymore, but the current state of this component,
-		// the context will have a getStore and setStore functions available then, because they were declared
-		// on the state of this component
-		return (
-			<Provider value={state}>
-				<PassedComponent {...props} />
-			</Provider>
-		);
-	};
+			});
+		}
+		componentDidMount() {
+			fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_diego")
+				.then(response => response.json())
+				.then(data => {
+					console.log(data);
+					let { store } = this.state;
+					this.setState({ store: { ...store, agenda: data } });
+				});
+		}
+		render() {
+			return (
+				<Provider value={this.state}>
+					<PassedComponent {...this.props} />
+				</Provider>
+			);
+		}
+	}
 	return StoreWrapper;
 };
 
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,10 +1,89 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contact: [],
+			contacts: [
+				{
+					id: 1,
+					full_name: "Diego Rojas",
+					email: "dsrt1991@gmail.com",
+					phone: "12345678",
+					address: "calle 12345"
+				}
+			]
 		},
+
 		actions: {
-			//(Arrow) Functions that update the Store
+			addContact: (name, address, phone, email) => {
+				console.log(
+					JSON.stringify({
+						agenda_slug: "agenda_diego",
+						full_name: name,
+						email: email,
+						phone: phone,
+						address: address
+					})
+				);
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "post",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						agenda_slug: "agenda_diego",
+						full_name: name,
+						email: email,
+						phone: phone,
+						address: address
+					})
+				})
+					.then(response => console.log(response.json()))
+					.then(() => {
+						fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_diego")
+							.then(response => response.json())
+							.then(data => {
+								console.log(data);
+								setStore({ agenda: data });
+							});
+					});
+			},
+			editContact: (name, address, phone, email, id, history) => {
+				console.log("$$$$", name, address, phone, email, id);
+				let store = getStore();
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "put",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						agenda_slug: "agenda_diego",
+						full_name: name,
+						email: email,
+						phone: phone,
+						address: address
+					})
+				})
+					.then(response => console.log(response.json()))
+					.then(() => {
+						fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_diego")
+							.then(response => response.json())
+							.then(data => {
+								console.log(data);
+								setStore({ agenda: data });
+							});
+					})
+					.then(() => history.push("/"));
+			},
+			deleteContact: id => {
+				console.log(id);
+				const store = getStore();
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "delete",
+					headers: { "Content-Type": "aplication/json" }
+				}).then(() => {
+					fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_diego")
+						.then(response => response.json())
+						.then(data => {
+							setStore({ agenda: data });
+						});
+				});
+			}
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
 	};
